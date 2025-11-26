b108fd9282d99cb206a41967ce18a19803899a05
alok-044 <ak2186501@gmail.com>
2025-11-26 11:39:20 +0530

    added hooks

---
+++ annaporna-web/src/hooks/useIntersectionObserver.js
@@ -0,0 +1,39 @@
+import { useState, useEffect, useRef } from 'react';
+
+/**
+ * 
+ * @param {object} options 
+ * @returns {[React.RefObject, boolean]} 
+ */
+export const useIntersectionObserver = (options) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const elementRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.unobserve(entry.target);
+        }
+      },
+      {
+        threshold: 0.1,
+        ...options,
+      }
+    );
+
+    const currentElement = elementRef.current;
+    if (currentElement) {
+      observer.observe(currentElement);
+    }
+
+    return () => {
+      if (currentElement) {
+        observer.unobserve(currentElement);
+      }
+    };
+  }, [options]);
+
+  return [elementRef, isVisible];
+};
