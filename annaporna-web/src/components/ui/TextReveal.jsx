f79c027a4e49970a875be9427b4b2b54e5be4d84
alok-044 <ak2186501@gmail.com>
2025-11-26 10:51:25 +0530

    Added ui components

---
+++ annaporna-web/src/components/ui/TextReveal.jsx
@@ -0,0 +1,17 @@
+const TextReveal = ({ text, className = "" }) => {
+  return (
+    <div className={`inline-block ${className}`}>
+       {text.split(" ").map((word, i) => (
+         <span 
+            key={i} 
+            className="inline-block animate-slide-up opacity-0"
+            style={{ animationDelay: `${i * 0.1}s` }}
+         >
+           {word}&nbsp;
+         </span>
+       ))}
+    </div>
+  );
+};
+
+export default TextReveal;
